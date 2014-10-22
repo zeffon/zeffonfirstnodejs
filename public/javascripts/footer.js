4293ea9fc241158769dcc929fc8d10f5605f4814
zeffon <zephoony@gmail.com>
2014-10-22 09:26:41 +0800

    let the returntop display in block with the stickUp

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -21,32 +21,12 @@ $(document).ready(
     }
     )
 
-//return the top
-$(function(){
-
-    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码
-
-        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
-
-        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示
-
-            $("#returnTop").fadeIn(400); //淡出
-
-        }else{
-
-            $("#returnTop").stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
-
-        }
-
+    $(function($) {
+        $(document).ready( function() {
+            //enabling stickUp on the '.navbar-wrapper' class
+            $('.navbar-wrapper').stickUp();
+        });
     });
-
-    $("#returnTop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
-
-        $("html,body").animate({scrollTop:"0px"},200);
-
-    });
-
-});
 
 
 
