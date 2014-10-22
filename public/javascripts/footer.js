d9cfd01df72f609681e04b42374de71c96e37787
zeffon <zephoony@gmail.com>
2014-10-22 12:54:19 +0800

    fix the footer

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -64,18 +64,18 @@ $(document).ready(
     }
 
 $(function(){
-    var btn=document.getElementById('returnTop');
+
     var d=document.documentElement;
     window.onscroll=set;
-    btn.onclick=function (){
-        btn.style.display="none";
+    $('#returnTop').onclick=function (){
+        this.style.display="none";
         window.onscroll=null;
         this.timer=setInterval(function(){
             d.scrollTop-=Math.ceil(d.scrollTop*0.1);
-            if(d.scrollTop==0) clearInterval(btn.timer,window.onscroll=set);
+            if(d.scrollTop==0) clearInterval(this.timer,window.onscroll=set);
         },10);
     };
-    function set(){btn.style.display=d.scrollTop?'block':"none"}
+    function set(){this.style.display=d.scrollTop?'block':"none"}
 });
 
 
