2dc037797aa6ff67e2b0fc0436949ade7f094419
zeffon <zephoony@gmail.com>
2014-10-22 09:07:54 +0800

    let the returntop display in block

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -19,30 +19,34 @@ $(document).ready(
     $(this).parent().addClass('active');
     });
     }
+    )
 
 //return the top
+$(function(){
 
-            (function(returnId) {
-                var returnTopBtn = document.getElementById(returnId);
-                var d = document.documentElement;
-                var b = document.body;
-                window.onscroll = set;
-                returnTopBtn.onclick = function() {
-                    returnTopBtn.style.display = "none";
-                    window.onscroll = null;
-                    this.timer = setInterval(function() {
-                            d.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
-                            b.scrollTop -= Math.ceil((d.scrollTop + b.scrollTop) * 0.1);
-                            if ((d.scrollTop + b.scrollTop) == 0) clearInterval(returnId.timer, window.onscroll = set);
-                        },
-                        10);
-                };
-                function set() {
-                    returnId.style.display = (d.scrollTop + b.scrollTop > 100) ? 'block': "none"
-                }
-            })('returnTop')
-//        returnTop('returnTop');
-    );
+    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码
+
+        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
+
+        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示
+
+            $("#returnTop").fadeIn(400); //淡出
+
+        }else{
+
+            $("#returnTop").stop().fadeOut(400); //如果返回或者没有超过,就淡入.必须加上stop()停止之前动画,否则会出现闪动
+
+        }
+
+    });
+
+    $("#returnTop").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+
+        $("html,body").animate({scrollTop:"0px"},200);
+
+    });
+
+});
 
 
 
