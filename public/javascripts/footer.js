42590d349c33a2fe0785dd04018e1ed180dfcafc
zeffon <zephoony@gmail.com>
2014-10-22 20:47:00 +0800

    add the duoshuo

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -49,6 +49,18 @@ $(document).ready(
     )
 
 
+var duoshuoQuery = {short_name:"zeffonfirstnodejs"};
+(function() {
+    var ds = document.createElement('script');
+    ds.type = 'text/javascript';ds.async = true;
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    (document.getElementsByTagName('head')[0]
+    || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+
+<!-- 多说公共JS代码 end -->
+
 
 
 
